3e2a9480d1d493bd01f4c87608e1b12378dfc42e
Mayya Krivopalova <m.krivopalova@onpoint.ru>
2023-12-29 23:59:06 +0300

    init

---
+++ script.js
@@ -0,0 +1,286 @@
+// надписи и цвета на секторах
+const prizes = [
+  {
+    text: "Идея",
+    name: "idea",
+    type: "text",
+    color: "hsl(197 30% 43%)",
+  },
+  {
+    text: "Мем",
+    name: "meme",
+    type: "image",
+    color: "hsl(173 58% 39%)",
+  },
+  {
+    text: "Шутка",
+    name: "joke",
+    type: "text",
+    color: "hsl(43 74% 66%)",
+  },
+  {
+    text: "Блог в подарок",
+    color: "hsl(12 76% 61%)",
+  },
+  {
+    text: "Скидок нет",
+    color: "hsl(350 60% 52%)",
+  },
+  {
+    text: "Таргет в подарок",
+    color: "hsl(91 43% 54%)",
+  },
+  {
+    text: "Скидка 30% на всё",
+    color: "hsl(140 36% 74%)",
+  },
+];
+
+// создаём переменные для быстрого доступа ко всем объектам на странице — блоку в целом, колесу, кнопке и язычку
+const wheel = document.querySelector(".deal-wheel");
+const spinner = wheel.querySelector(".spinner");
+const trigger = wheel.querySelector(".btn-spin");
+const ticker = wheel.querySelector(".ticker");
+
+const bing = new Audio("mp3/bing.mp3");
+const bells = new Audio("mp3/music.mp3");
+
+// на сколько секторов нарезаем круг
+const prizeSlice = 360 / prizes.length;
+// на какое расстояние смещаем сектора друг относительно друга
+const prizeOffset = Math.floor(180 / prizes.length);
+// прописываем CSS-классы, которые будем добавлять и убирать из стилей
+const spinClass = "is-spinning";
+const selectedClass = "selected";
+// получаем все значения параметров стилей у секторов
+const spinnerStyles = window.getComputedStyle(spinner);
+
+// переменная для анимации
+let tickerAnim;
+// угол вращения
+let rotation = 0;
+// текущий сектор
+let currentSlice = 0;
+// переменная для текстовых подписей
+let prizeNodes;
+
+// расставляем текст по секторам
+const createPrizeNodes = () => {
+  // обрабатываем каждую подпись
+  prizes.forEach(({ text, color, reaction }, i) => {
+    // каждой из них назначаем свой угол поворота
+    const rotation = prizeSlice * i * -1 - prizeOffset;
+    // добавляем код с размещением текста на страницу в конец блока spinner
+    spinner.insertAdjacentHTML(
+      "beforeend",
+      // текст при этом уже оформлен нужными стилями
+      `<li class="prize" data-reaction=${reaction} style="--rotate: ${rotation}deg">
+          <span class="text">${text}</span>
+        </li>`
+    );
+  });
+};
+
+// рисуем разноцветные секторы
+const createConicGradient = () => {
+  // устанавливаем нужное значение стиля у элемента spinner
+  spinner.setAttribute(
+    "style",
+    `background: conic-gradient(
+        from -90deg,
+        ${prizes
+          // получаем цвет текущего сектора
+          .map(
+            ({ color }, i) =>
+              `${color} 0 ${(100 / prizes.length) * (prizes.length - i)}%`
+          )
+          .reverse()}
+      );`
+  );
+};
+
+// создаём функцию, которая нарисует колесо в сборе
+const setupWheel = () => {
+  // сначала секторы
+  createConicGradient();
+  // потом текст
+  createPrizeNodes();
+  // а потом мы получим список всех призов на странице, чтобы работать с ними как с объектами
+  prizeNodes = wheel.querySelectorAll(".prize");
+};
+
+// определяем количество оборотов, которое сделает наше колесо
+const spinertia = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+// функция запуска вращения с плавной остановкой
+const runTickerAnimation = () => {
+  // взяли код анимации отсюда: https://css-tricks.com/get-value-of-css-rotation-through-javascript/
+  const values = spinnerStyles.transform.split("(")[1].split(")")[0].split(",");
+  const a = values[0];
+  const b = values[1];
+  let rad = Math.atan2(b, a);
+
+  if (rad < 0) rad += 2 * Math.PI;
+
+  const angle = Math.round(rad * (180 / Math.PI));
+  const slice = Math.floor(angle / prizeSlice);
+
+  // анимация язычка, когда его задевает колесо при вращении
+  // если появился новый сектор
+  if (currentSlice !== slice) {
+    bing.play();
+    // убираем анимацию язычка
+    ticker.style.animation = "none";
+    // и через 10 миллисекунд отменяем это, чтобы он вернулся в первоначальное положение
+    setTimeout(() => (ticker.style.animation = null), 10);
+    // после того, как язычок прошёл сектор - делаем его текущим
+    currentSlice = slice;
+  }
+  // запускаем анимацию
+  tickerAnim = requestAnimationFrame(runTickerAnimation);
+};
+
+// функция выбора призового сектора
+const selectPrize = () => {
+  bells.play();
+  const selected = Math.floor(rotation / prizeSlice);
+  prizeNodes[selected].classList.add(selectedClass);
+};
+
+// отслеживаем нажатие на кнопку
+trigger.addEventListener("click", () => {
+  // делаем её недоступной для нажатия
+  trigger.disabled = true;
+  // задаём начальное вращение колеса
+  rotation = Math.floor(Math.random() * 360 + spinertia(2000, 5000));
+  // убираем прошлый приз
+  prizeNodes.forEach((prize) => prize.classList.remove(selectedClass));
+  // добавляем колесу класс is-spinning, с помощью которого реализуем нужную отрисовку
+  wheel.classList.add(spinClass);
+  // через CSS говорим секторам, как им повернуться
+  spinner.style.setProperty("--rotate", rotation);
+  // возвращаем язычок в горизонтальную позицию
+  ticker.style.animation = "none";
+  // запускаем анимацию вращение
+  runTickerAnimation();
+});
+
+// отслеживаем, когда закончилась анимация вращения колеса
+spinner.addEventListener("transitionend", () => {
+  // останавливаем отрисовку вращения
+  cancelAnimationFrame(tickerAnim);
+  // получаем текущее значение поворота колеса
+  rotation %= 360;
+  // выбираем приз
+  selectPrize();
+  // убираем класс, который отвечает за вращение
+  wheel.classList.remove(spinClass);
+  // отправляем в CSS новое положение поворота колеса
+  spinner.style.setProperty("--rotate", rotation);
+  // делаем кнопку снова активной
+  trigger.disabled = false;
+});
+
+// подготавливаем всё к первому запуску
+setupWheel();
+
+// --- sky ---
+
+(function () {
+  var COUNT = 300;
+  var masthead = document.querySelector(".sky");
+  var canvas = document.createElement("canvas");
+  var ctx = canvas.getContext("2d");
+  var width = masthead.clientWidth;
+  var height = masthead.clientHeight;
+  var i = 0;
+  var active = false;
+
+  function onResize() {
+    width = masthead.clientWidth;
+    height = masthead.clientHeight;
+    canvas.width = width;
+    canvas.height = height;
+    ctx.fillStyle = "#FFF";
+
+    var wasActive = active;
+    active = width > 600;
+
+    if (!wasActive && active) requestAnimFrame(update);
+  }
+
+  var Snowflake = function () {
+    this.x = 0;
+    this.y = 0;
+    this.vy = 0;
+    this.vx = 0;
+    this.r = 0;
+
+    this.reset();
+  };
+
+  Snowflake.prototype.reset = function () {
+    this.x = Math.random() * width;
+    this.y = Math.random() * -height;
+    this.vy = 1 + Math.random() * 3;
+    this.vx = 0.5 - Math.random();
+    this.r = 1 + Math.random() * 2;
+    this.o = 0.5 + Math.random() * 0.5;
+  };
+
+  canvas.style.position = "absolute";
+  canvas.style.left = canvas.style.top = "0";
+
+  var snowflakes = [],
+    snowflake;
+  for (i = 0; i < COUNT; i++) {
+    snowflake = new Snowflake();
+    snowflake.reset();
+    snowflakes.push(snowflake);
+  }
+
+  function update() {
+    ctx.clearRect(0, 0, width, height);
+
+    if (!active) return;
+
+    for (i = 0; i < COUNT; i++) {
+      snowflake = snowflakes[i];
+      snowflake.y += snowflake.vy;
+      snowflake.x += snowflake.vx;
+
+      ctx.globalAlpha = snowflake.o;
+      ctx.beginPath();
+      ctx.arc(snowflake.x, snowflake.y, snowflake.r, 0, Math.PI * 2, false);
+      ctx.closePath();
+      ctx.fill();
+
+      if (snowflake.y > height) {
+        snowflake.reset();
+      }
+    }
+
+    requestAnimFrame(update);
+  }
+
+  // shim layer with setTimeout fallback
+  window.requestAnimFrame = (function () {
+    return (
+      window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      function (callback) {
+        window.setTimeout(callback, 1000 / 60);
+      }
+    );
+  })();
+
+  onResize();
+  window.addEventListener("resize", onResize, false);
+
+  masthead.appendChild(canvas);
+})();
